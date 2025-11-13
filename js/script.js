db30204885dbc8adeccf2f76cf8eeaafa78ddfbb
rosendo <rosendc30@gmail.com>
2025-11-12 21:58:56 -0300

    teste toogle - ok

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ const statusBTN = document.getElementById('statusBTN');
 
     checkbox.addEventListener('change', function () {
       statusBTN.textContent = this.checked ? 'Repetir números' : 'Não repetir números';
-      
+      console.log('Checkbox is now: ' + (this.checked ? 'checked' : 'unchecked'));
     });
