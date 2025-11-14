23778a39b83c3b5ab20c62d9cd26138192d90440
rosendo <rosendc30@gmail.com>
2025-11-13 21:44:03 -0300

    impedindo comportamento do form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const checkbox = document.getElementById('toggleBtn');
 const statusBTN = document.getElementById('statusBTN');
+const form  = document.querySelector('form');
+
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); 
+    });
 
     checkbox.addEventListener('change', function () {
       statusBTN.textContent = this.checked ? 'Repetir números' : 'Não repetir números';
